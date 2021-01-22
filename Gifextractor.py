340e37e469c2531a612ef9c62f1be96cf5c44e5d
Jake8796 <57572177+jake8796@users.noreply.github.com>
2021-01-22 12:25:53 -0700

    Update Gifextractor.py

--- Gifextractor.py
+++ Gifextractor.py
@@ -1,108 +1,131 @@
 from PIL import Image
 import os
-from PIL import GifImagePlugin
+from statistics import mode
+import webbrowser
 
-def analyseImage(path):
-    '''
-    Pre-process pass over the image to determine the mode (full or additive).
-    Necessary as assessing single frames isn't reliable. Need to know the mode 
-    before processing all frames.
-    '''
-    im = Image.open(path)
-    results = {
-        'size': im.size,
-        'mode': 'full',
-    }
-    try:
-        while True:
-            if im.tile:
-                tile = im.tile[0]
-                update_region = tile[1]
-                update_region_dimensions = update_region[2:]
-                if update_region_dimensions != im.size:
-                    results['mode'] = 'partial'
-                    break
-            im.seek(im.tell() + 1)
-    except EOFError:
-        pass
-    return results
+path = 'wavy'
+os.chdir(r"C:\Users\Jacob\Desktop\Projects\Gifmaker")
 
-def processImage(path):
-    '''
-    Iterate the GIF, extracting each frame.
-    '''
-    mode = analyseImage(path)['mode']
-    
-    im = Image.open(path)
+num_key_frames = 60
 
-    i = 0
-    p = im.getpalette()
-    last_frame = im.convert('RGBA')
-    
-    try:
-        while True:
-            print ("saving %s (%s) frame %d, %s %s" % (path, mode, i, im.size, im.tile))
-            
-            '''
-            If the GIF uses local colour tables, each frame will have its own palette.
-            If not, we need to apply the global palette to the new frame.
-            '''
-            if not im.getpalette():
-                im.putpalette(p)
-            
-            new_frame = Image.new('RGBA', im.size)
-            
-            '''
-            Is this file a "partial"-mode GIF where frames update a region of a different size to the entire image?
-            If so, we need to construct the new frame by pasting it on top of the preceding frames.
-            '''
-            if mode == 'partial':
-                new_frame.paste(last_frame)
-            
-            new_frame.paste(im, (0,0), im.convert('RGBA'))
-            new_frame.save('%s-%d.png' % (''.join(os.path.basename(path).split('.')[:-1]), i), 'PNG')
+with Image.open(path+'.gif') as im:
+    if im.n_frames < num_key_frames:
+        num_key_frames = im.n_frames
+    for i in range(num_key_frames):
+        im.seek(im.n_frames // num_key_frames * i)
+        im.save('{}.png'.format(i))
 
-            i += 1
-            last_frame = new_frame
-            im.seek(im.tell() + 1)
-    except EOFError:
-        pass
-
+#Height of your neopixel display
 width = 16
 height = 16
-path = "dots"
-otherPath = (path + ".gif")
-directory = r'C:\Users\Jacob\Desktop\Gifmaker'
-os.chdir(directory)
+
+#Crops from the center of the gif
+zoom = 500//2
+(left,upper,right,lower) = (int(im.width//2-zoom),int( im.height-zoom),int( im.width//2+zoom),int( im.height))
 
 
-analyseImage(otherPath)
-processImage(otherPath)
+print("\n** Analysing image **\n")
 
-fileName = (path + ".txt")
-f = open(fileName,"w")
-im = Image.open(otherPath)
-frames = im.n_frames
-if frames > 65:
-    frames = 65
+# Display image format, size, colour mode
+print("Format:", im.format, "\nWidth:", im.width, "\nHeight:", im.height, "\nMode:", im.mode)
 
 
-for frame in range(0,frames):
-   res = (path + "-%d.png") % frame 
-   cock = Image.open(res)
-   cock = cock.resize((width,height))
-   imRGB = cock.convert('RGB')
-   
-   for j in range(width):
-       for k in range(height):
-            data = imRGB.getpixel((j,k))
-            if data < (60,60,60):
-              data = (0,0,0)
-            f.write("leds[%d][%d]" % (j,k))
-            f.write("= CRGB%s;\n" % (data,))
-            if j == width-1 and k == height-1:
-                f.write("FastLED.show();\ndelay(60);\n")
-                imRGB.close()
+# Check if GIF is animated
+
+print("Number of frames: " + str(im.n_frames))
+
+# Iterate through frames and pixels, top row first
+
+listA = []
+#Finds the pixel color that could be background of a gif and remove it
+with Image.open('0.png') as img:
+    rgb_img= img.convert('RGB')
+    for y in range(img.height):
+        for x in range(img.width): 
+            data = rgb_img.getpixel((x, y))
+            listA.append(data)
+
+remove = mode(listA)
+print("Pixel removed:", remove)
+
+# Opens Arduino File
+os.chdir(r"C:\Users\Jacob\Desktop\Projects\Gifmaker\code")
+with open("Intro.txt") as f:
+    with open("code.ino", "w") as f1:
+        for line in f:
+            f1.write(line) 
+f = open('code.ino','a')
+for z in range(0,num_key_frames):
+
+    # Go to frame
+    os.chdir(r"C:\Users\Jacob\Desktop\Projects\Gifmaker")
+    im = Image.open(str(z)+'.png')
+    #im = im.crop((left,upper,right,lower)) #(left, upper, right, lower) origin is at top left corner
+    im = im.resize((width,height))
+    im = im.rotate(270)
+    rgb_im = im.convert('RGB')
+    os.chdir(r"C:\Users\Jacob\Desktop\Projects\Gifmaker\code")
+    for y in range(width):
+        for x in range(height):
+            
+            # Get RGB values of each pixel
+            data = rgb_im.getpixel((x, y))
+            # Builds code in a txt file for Neopixel display
+            
+            if y == 0 and x == 0:
+               f.write('\nif(currentMillis - previousMillis >= fps && frame == %d) {\n' % z)
+               f.write('   previousMillis = currentMillis;\n')
+              
+            if y == width-1 and x == height-1 and z != num_key_frames-1:
+                f.write('   FastLED.show();\n   frame = frame+1;\n  clearLEDs();\n}\n ')
+               
+            elif y == width-1 and x == height-1 and z == num_key_frames-1:
+                f.write('   leds[%d][%d] = ' % (y,x))
+                f.write('   CRGB(%s,%s,%s);\n' % data)
+                f.write('   FastLED.show();\n   frame = 0;\n    clearLEDs();\n}\n ')
+                f.write('}\nvoid clearLEDs(){\nfor(int j = 0; j < 16; j++){\nfor(int k = 0; k < 16; k++){\nleds[j][k] = CRGB(0,0,0); \n }}}')
+                
+           
+            if data ==(0,0,0) or z == num_key_frames-1:
+                continue
+            f.write('   leds[%d][%d] = ' % (y,x))
+            f.write('   CRGB(%s,%s,%s);\n' % data)
+os.chdir(r"C:\Users\Jacob\Desktop\Projects\Gifmaker")
+f1 = open(path+'1.txt','w')
+for z in range(0,num_key_frames):
+
+    # Go to frame
+    os.chdir(r"C:\Users\Jacob\Desktop\Projects\Gifmaker")
+    im = Image.open(str(z)+'.png')
+    #im = im.crop((left,upper,right,lower)) #(left, upper, right, lower) origin is at top left corner
+    im = im.resize((width,height))
+    im = im.rotate(270)
+    rgb_im = im.convert('RGB')
+    for y in range(width):
+        for x in range(height):
+            
+            # Get RGB values of each pixel
+            data = rgb_im.getpixel((x, y))
+            # Builds code in a txt file for Neopixel display
+            
+            if y == 0 and x == 0:
+               f1.write('\nif(currentMillis - previousMillis >= fps && frame == %d) {\n' % z)
+               f1.write('   previousMillis = currentMillis;\n')
+              
+            if y == width-1 and x == height-1 and z != num_key_frames-1:
+                f1.write('   FastLED.show();\n   frame = frame+1;\n  clearLEDs();\n}\n ')
+               
+            elif y == width-1 and x == height-1 and z == num_key_frames-1:
+                f1.write('   FastLED.show();\n   frame = 0;\n    clearLEDs();\n}\n ')
+                f1.write('}\nvoid clearLEDs(){\nfor(int j = 0; j < 16; j++){\nfor(int k = 0; k < 16; k++){\nleds[j][k] = CRGB(0,0,0); \n }}}')
+                
+           
+            if data ==(0,0,0) or data == remove:
+                continue
+            f1.write('   leds[%d][%d] = ' % (y,x))
+            f1.write('   CRGB(%s,%s,%s);\n' % data)
+os.chdir(r"C:\Users\Jacob\Desktop\Projects\Gifmaker\code")
+webbrowser.open('code.ino')
 
 
 
